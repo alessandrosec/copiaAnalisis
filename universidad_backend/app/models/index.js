c9a95d2a65bf9440985204639482847963028d00
Alessandro Sec <71790279+alessandrosec@users.noreply.github.com>
2025-09-09 11:55:30 -0600

    Carga de archivos de reposirotio orgin

---
+++ universidad_backend/app/models/index.js
@@ -0,0 +1,39 @@
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  port: dbConfig.port,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+try {
+  db.usuarios = require("./usuario.model.js")(sequelize, Sequelize);
+  console.log("✅ Modelo 'usuario' cargado correctamente.");
+} catch (err) {
+  console.error("❌ Error al cargar modelo 'usuario':", err.message);
+}
+
+try {
+  db.docentes = require("./estudiante.model.js")(sequelize, Sequelize);
+  console.log("✅ Modelo 'estudiante' cargado correctamente.");
+} catch (err) {
+  console.error("❌ Error al cargar modelo 'estudiante':", err.message);
+}
+module.exports = db;
